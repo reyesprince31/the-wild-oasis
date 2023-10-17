de6ef1ceeaa2616a003e366236a197abb558b4ad
Prince Reyes <krayzerprince@gmail.com>
2023-10-17 13:07:02 +0800

    Add Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/Globalstyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello World</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => console.log("Hello")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="medium"
+                onClick={() => console.log("Out")}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
