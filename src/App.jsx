89fd5e8ff51ae8b7a3b1bb3870b8f7cce28d8da2
Prince Reyes <krayzerprince@gmail.com>
2023-10-17 14:28:29 +0800

    Add and implement React-Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/Globalstyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const StyledApp = styled.div`
-  /* background-color: orangered; */
-  padding: 20px;
-`;
+import GlobalStyles from "./styles/Globalstyles";
+
+import {
+  Dashboard,
+  Bookings,
+  Cabins,
+  Users,
+  Settings,
+  Account,
+  Login,
+  PageNotFound,
+} from "./pages/index";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => console.log("Hello")}>Check in</Button>
-              <Button
-                variation="secondary"
-                size="medium"
-                onClick={() => console.log("Out")}>
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Dashboard from "./Dashboard";
+import Bookings from "./Bookings";
+import Cabins from "./Cabins";
+import Users from "./Users";
+import Settings from "./Settings";
+import Account from "./Account";
+import Login from "./Login";
+import PageNotFound from "./PageNotFound";
+
+export {
+  Dashboard,
+  Bookings,
+  Cabins,
+  Users,
+  Settings,
+  Account,
+  Login,
+  PageNotFound,
+};
