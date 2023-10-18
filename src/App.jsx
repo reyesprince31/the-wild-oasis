f5724b870b497f626c2ac86add2c8d22f653f67f
Prince Reyes <krayzerprince@gmail.com>
2023-10-18 20:50:41 +0800

    Fetch Cabin data, install date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
