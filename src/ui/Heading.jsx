7d9877561e534cd7a7465214a565b66b4ed77c7c
Prince Reyes <krayzerprince@gmail.com>
2023-11-10 12:38:57 +0800

    Add user Login Auth with supabase

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -20,13 +20,21 @@ const Heading = styled.h1`
       font-weight: 600;
     `}
 
-    ${(props) =>
+  ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
     `}
 
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 500;
+      text-align: center;
+    `}
+  
   line-height: 1.4;
 `;
 
