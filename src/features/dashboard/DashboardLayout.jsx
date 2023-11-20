eff13dc56f64cde965144701af2b82d21452eec2
Prince Reyes <krayzerprince@gmail.com>
2023-11-20 08:42:18 +0800

    work on displaying stays for current day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,12 +19,7 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { cabins, isLoading3 } = useCabins();
   const { isLoading: isLoading1, bookings } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isLoading: isLoading2,
-    numDays,
-  } = useRecentStays();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
@@ -35,8 +31,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Statistics</div>
-      <div>Today's activity</div>
+
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
